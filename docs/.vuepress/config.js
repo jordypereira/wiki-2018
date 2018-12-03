d185352395e90ba43736d4903083dc523707a0d1
Jordy Pereira <jordy.pereira@hotmail.com>
2018-12-03 14:10:51 +0100

    Design for emotion chapter 1 and 2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,7 +24,7 @@ module.exports = {
       {
         title: 'Articles',
         collapsable: false,
-        children: ['/articles/Neuro-Web-Design-What-Makes-Them-Click/'],
+        children: ['/articles/Neuro-Web-Design-What-Makes-Them-Click/', '/articles/designing-for-emotion/'],
       },
       {
         title: 'Testing',
